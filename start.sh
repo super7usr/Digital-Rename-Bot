a183cb23554dd9327dfd645a512607aefdc2046a
super7usr <glenna.lakin@blenched.com>
2025-03-23 07:57:50 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 echo "Cloning Jenna Repository"
 git clone https://github.com/super7usr/Digital-Rename-Bot /rename
 cd /rename
+brew tap heroku/brew && brew install heroku
+sudo snap install --classic heroku
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 pip install ffmpeg
